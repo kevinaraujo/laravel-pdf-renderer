2a64cd4fe14543a474c64c74a5886377b9e159c1
Kevin Araújo <kfa_34@hotmail.com>
2020-03-24 17:26:32 -0300

    added template in view directory

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -8,7 +8,7 @@ class IndexController extends Controller
 {
     public function render()
     {
-        $path = public_path() . '/../storage/app/template.blade.php';
+        $path = public_path() . '/../resources/views/template.blade.php';
         $pdf = PDF::loadFile($path);
 
         return $pdf->stream('contrato-aluno-4106698796.pdf');
---
+++ resources/views/template.blade.php
@@ -0,0 +1,16 @@
+<!--DEFAULT CODE SO THAT LIB WORKS-->
+<style>@page{header: page-header;footer:page-footer; } htmlpageheader{ font-size: 12px;}</style>
+<htmlpageheader name="page-header">Contrato Pravaler SA</htmlpageheader>
+<htmlpagefooter name="page-footer">Página {PAGENO}</htmlpagefooter>
+<!--TEMPLATE BEGINNING-->
+<table border="1" cellpadding="0" cellspacing="0" width="100%" class="TabelaCollapse" style="font-size: 14px">
+    <tbody>
+    <tr>
+        <td colspan="18" align="center" bgcolor="#F2F2F2"><strong>QUADRO I</strong></td>
+    </tr>
+    <tr>
+        <td colspan="18" align="center" bgcolor="#F2F2F2"><strong>1.1. CREDORA – INSTITUIÇÃO DE ENSINO (IES) </strong></td>
+    </tr>
+    <tr>
+
+</table>
