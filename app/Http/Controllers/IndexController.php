5167b669efa0054824e5108b61e4abfeecf65854
Kevin Araújo <kfa_34@hotmail.com>
2020-03-24 12:43:53 -0300

    laravel-dompdf

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\App;
+
+class IndexController extends Controller
+{
+    public function render()
+    {
+        $pdf = App::make('dompdf.wrapper');
+
+        $html = file_get_contents(getcwd() .'/../storage/app/template.blade.php');
+        $pdf->loadHTML($html);
+
+        return $pdf->stream();
+    }
+}
