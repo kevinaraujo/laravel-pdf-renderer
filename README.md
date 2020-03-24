06db8255ff246a5d042dc17912e341954a4ed155
Kevin Araújo <kfa_34@hotmail.com>
2020-03-24 17:58:57 -0300

    refactored readme

--- README.md
+++ README.md
@@ -4,8 +4,8 @@ Run on command line `php artisan serve --port=8080` and later enter into
 `http://localhost:8080/render` on browser
 
 I used two libs to render PDF:
-- https://github.com/niklasravnsborg/laravel-pdf
-- https://github.com/barryvdh/laravel-dompdf
+- [niklasravnsborg/laravel-pdf](https://github.com/niklasravnsborg/laravel-pdf)
+- [barryvdh/laravel-dompdf](https://github.com/barryvdh/laravel-dompdf)
 
-These are separated by branch and if you want swapping between then just checkout the branch
+These are separated by branch and if you want swapping between them just checkout the branch
 and run `composer update` on command line.
--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -1,8 +1,21 @@
 <!--DEFAULT CODE SO THAT LIB WORKS-->
-<style>@page{header: page-header;footer:page-footer; } htmlpageheader{ font-size: 12px;}</style>
-<htmlpageheader name="page-header">Contrato Pravaler SA</htmlpageheader>
+<style>@page{margin-top: 80px; header: page-header;footer:page-footer; }</style>
+<center><htmlpageheader name="page-header">Contrato Pravaler SA</htmlpageheader></center>
 <htmlpagefooter name="page-footer">Página {PAGENO}</htmlpagefooter>
 <!--TEMPLATE BEGINNING-->
+<table border="1" cellpadding="0" cellspacing="0" width="100%" class="TabelaCollapse" style="font-size: 14px">
+    <tbody>
+    <tr>
+        <td colspan="18" align="center" bgcolor="#F2F2F2"><strong>QUADRO I</strong></td>
+    </tr>
+    <tr>
+        <td colspan="18" align="center" bgcolor="#F2F2F2"><strong>1.1. CREDORA – INSTITUIÇÃO DE ENSINO (IES) </strong></td>
+    </tr>
+    <tr>
+
+</table>
+<pagebreak>
+
 <table border="1" cellpadding="0" cellspacing="0" width="100%" class="TabelaCollapse" style="font-size: 14px">
     <tbody>
     <tr>
