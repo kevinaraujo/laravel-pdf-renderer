5e79d4d2dd8dd03ae7abbcb47f0597a36d81f803
Kevin Araújo <kfa_34@hotmail.com>
2020-03-24 12:45:32 -0300

    initial commit

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,17 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\App;
-
 class IndexController extends Controller
 {
     public function render()
     {
-        $pdf = App::make('dompdf.wrapper');
-
-        $html = file_get_contents(getcwd() .'/../storage/app/template.blade.php');
-        $pdf->loadHTML($html);
-
-        return $pdf->stream();
+        die('ok');
     }
 }
