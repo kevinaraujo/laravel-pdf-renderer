c4465eb74ad0e85b6f49aad70462a06390ef72f9
Kevin Araújo <kfa_34@hotmail.com>
2020-03-24 15:30:25 -0300

    working on it

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,10 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use PDF;
+
 class IndexController extends Controller
 {
     public function render()
     {
-        die('ok');
+        Mail
+        $data = [
+            'foo' => 'bar'
+        ];
+        $pdf = Pdf(public_path() . '/../storage/app/template.blade.php', $data);
+        return $pdf->stream('document.pdf');
     }
 }
